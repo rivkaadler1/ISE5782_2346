3d6f932f9b3e46a777a69128d694e2ea3a26140b
rivkaadler1 <adlerrivka66@gmail.com>
2022-04-23 23:24:13 +0300

    finish exercise 4

--- ISE5782_2346/src/primitives/Point.java
+++ ISE5782_2346/src/primitives/Point.java
@@ -96,4 +96,6 @@ public class Point
 	{	
 		return xyz.d3;
 	}
+
+	
 }
---
+++ ISE5782_2346/src/renderer/Camera.java
@@ -0,0 +1,274 @@
+package renderer;
+import static primitives.Util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+public class Camera 
+{
+	private Point p0; //location of the camera
+	private Vector vUp;
+	private Vector vTo;
+	private Vector vRight;
+	private double width;
+	private double height;
+	private double distance;
+
+	/**
+	 * This constructor that create new camera
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @param vTo Vector value
+	 * @param vUp Vector value
+	 * @param p0 Point3D value
+	 * @return Camera
+	 * @throws Exception 
+	 */
+	public Camera(Point p0, Vector vTo, Vector vUp) throws IllegalArgumentException 
+	{
+		if(!isZero(vTo.dotProduct(vUp))) // if vTo doesn't orthogonal to vUp
+			throw new IllegalArgumentException("vUp doesnt ortogonal to vTo");
+		
+		//all the vectors need to be normalize:
+		this.vTo = vTo.normalize();
+		this.vUp = vUp.normalize();
+		vRight = (vTo.crossProduct(vUp)).normalize();
+		
+		this.p0 = p0;
+
+	}
+	
+	/**
+	 * Update function for View Plane size  
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @param width double value
+	 * @param height double value
+	 * @return Camera	 
+	 */
+	public Camera setViewPlaneSize(double width, double height)
+	{
+		this.width = width;
+		this.height = height;
+		return this;
+	}
+	
+	
+	/**
+	 * Update function for distance
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @param distance double value
+	 * @return Camera	 
+	 */
+	public Camera setDistance(double distance)
+	{
+		this.distance = distance;
+		return this;
+	}
+
+	/**
+	 * The function is responsible for creating the rays from the camera
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @param nX int value - resolution of pixel in X
+	 * @param nY int value - resolution of pixel in Y
+	 * @param j int value - index of column
+	 * @param i int value - index of row
+	 * @return Ray that created	 
+	 * @throws Exception 
+	 */
+	public Ray constructRayThroughPixel(int nX, int nY, int j, int i ) 
+	{
+		Point Pc;
+		if (isZero(distance))
+			Pc=p0;
+		else
+			Pc=p0.add(vTo.scale(distance));
+		
+		double Ry= height/nY;
+		double Rx=width/nX;
+		double Yi=(i-(nY-1)/2d)*Ry;
+		double Xj=(j-(nX-1)/2d)*Rx;
+		
+		if(isZero(Xj) && isZero(Yi))
+			return new Ray (p0, Pc.subtract(p0));
+		
+		Point Pij = Pc;
+		
+		if(!isZero(Xj))
+			Pij = Pij.add(vRight.scale(Xj));
+		
+		if(!isZero(Yi))
+			Pij = Pij.add(vUp.scale(-Yi));
+		
+		Vector Vij = Pij.subtract(p0);
+		
+		if(Pij.equals(p0))
+			return new Ray(p0, new Vector(Pij.getX(),Pij.getY(),Pij.getZ()));
+		return new Ray(p0, Vij);
+
+	}
+	
+	
+	
+	public List<Ray> constructBeamThroughPixel (int nX, int nY, int j, int i, int raysAmount)
+	{
+//		//æøé÷ú ëîåú ùì ÷øðééí ìúåê öåøú âøéã
+//		List<Ray> Rays = new ArrayList<Ray>();
+		int numOfRays = (int)Math.floor(Math.sqrt(raysAmount)); //num of rays in each row or column
+		
+		if (numOfRays==1) 
+			return List.of(constructRayThroughPixel(nX, nY, j, i));
+//
+		Point Pc;
+		if (isZero(distance))
+			Pc=p0;
+		else
+			Pc=p0.add(vTo.scale(distance));
+		
+		double Ry= height/nY;
+		double Rx=width/nX;
+		double Yi=(i-(nY-1)/2d)*Ry;
+		double Xj=(j-(nX-1)/2d)*Rx;
+//        
+        double PRy = Ry / numOfRays; //height distance between each ray
+        double PRx = Rx / numOfRays; //width distance between each ray
+//        
+	      //The distance between the screen and the camera cannot be 0
+        if (isZero(distance))
+        {
+            throw new IllegalArgumentException("distance cannot be 0");
+        }
+
+        List<Ray> sampleRays = new ArrayList<>();
+
+
+        for (int row = 0; row < numOfRays; ++row) 
+        {//foreach place in the pixel grid
+            for (int column = 0; column < numOfRays; ++column)
+            {
+                sampleRays.add(constructRaysThroughPixel(PRy,PRx,Yi, Xj, row, column));//add the ray
+            }
+        }
+        sampleRays.add(constructRayThroughPixel(nX, nY, j, i));//add the center screen ray
+        return sampleRays;
+	}
+	
+	 /**
+     * In this function we treat each pixel like a little screen of its own and divide it to smaller "pixels".
+     * Through each one we construct a ray. This function is similar to ConstructRayThroughPixel.
+     * @param Ry height of each grid block we divided the pixel into
+     * @param Rx width of each grid block we divided the pixel into
+     * @param yi distance of original pixel from (0,0) on Y axis
+     * @param xj distance of original pixel from (0,0) on X axis
+     * @param j j coordinate of small "pixel"
+     * @param i i coordinate of small "pixel"
+     * @param distance distance of screen from camera
+     * @return beam of rays through pixel
+     */
+    private Ray constructRaysThroughPixel(double Ry,double Rx, double yi, double xj, int j, int i)
+    {
+        Point Pc = p0.add(vTo.scale(distance)); //the center of the screen point
+
+        double ySampleI =  (i *Ry + Ry/2d); //The pixel starting point on the y axis
+        double xSampleJ=   (j *Rx + Rx/2d); //The pixel starting point on the x axis
+
+        Point Pij = Pc; //The point at the pixel through which a beam is fired
+        //Moving the point through which a beam is fired on the x axis
+        if (!isZero(xSampleJ + xj))
+        {
+            Pij = Pij.add(vRight.scale(xSampleJ + xj));
+        }
+        //Moving the point through which a beam is fired on the y axis
+        if (!isZero(ySampleI + yi))
+        {
+            Pij = Pij.add(vUp.scale(-ySampleI -yi ));
+        }
+        Vector Vij = Pij.subtract(p0);
+        
+        return new Ray(p0,Vij);//create the ray throw the point we calculate here
+    }
+	
+
+	/**
+	 * Getter for p0
+	 * 
+	 * @author sarit silverstone and rivki adler
+	 * @return Point value for p0	 
+	 */
+	public Point getP0() 
+	{
+		return p0;
+	}
+
+	/**
+	 * Getter for vUp
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @return Vector value for vUp	 
+	 */
+	public Vector getvUp() 
+	{
+		return vUp;
+	}
+
+	/**
+	 * Getter for vTo
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @return Vector value for vTo	 
+	 */
+	public Vector getvTo() 
+	{
+		return vTo;
+	}
+	
+	/**
+	 * Getter for vRight
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @return Vector value for vRight	 
+	 */
+	public Vector getvRight() 
+	{
+		return vRight;
+	}
+
+	/**
+	 * Getter for width
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @return double value for width	 
+	 */
+	public double getWidth() 
+	{
+		return width;
+	}
+
+	/**
+	 * Getter for height
+	 * 
+	 * @author  sarit silverstone and rivki adler
+	 * @return double value for height	 
+	 */
+	public double getHeight() 
+	{
+		return height;
+	}
+
+	/**
+	 * Getter for distance
+	 * 
+	 * @author sarit silverstone and rivki adler
+	 * @return double value for distance	 
+	 */
+	public double getDistance() 
+	{
+		return distance;
+	}
+}
